d7e892c73094b5cbf6daff0594e414c253b97928
Kostya Romanovskyi <romanovskiyk97@gmail.com>
2023-05-10 16:05:12 +0300

    update validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -8,9 +8,12 @@ const { HttpError } = require("../../helpers");
 const router = express.Router();
 
 const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string()
+    .pattern(/^\(\d{3}\) \d{3}-\d{4}$/)
+    .message("Invalid phone number")
+    .required(),
 });
 
 router.get("/", async (req, res, next) => {
