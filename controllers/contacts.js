3a3c97e31a3f89962c320154fe87aa178a9bc9e0
Kostya-Romanovskyi <106028051+Kostya-Romanovskyi@users.noreply.github.com>
2023-05-24 20:26:20 +0300

    deleted comment

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -6,7 +6,6 @@ const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
-  //   const result = await Contact.find({ owner }, req.body);
   const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
     skip,
     limit,
